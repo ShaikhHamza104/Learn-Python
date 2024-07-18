af800d934de4b3c5542541718993bf074ca32819
ShaikhHamza104 <kmohdhamza10@gmail.com>
2024-07-18 18:00:46 +0530

    07_enumerate.py file added

---
+++ chapter 4/07_enumerate.py
@@ -0,0 +1,10 @@
+l=[1,2,3,4,5,6]
+for index,element in enumerate(l):
+    print(index,element)
+
+l2=["Rohan" ,"Rahol","Rohi","Roshan","Riha"]
+for index,ele in enumerate(l2):
+    if index==0:
+        print("hi",ele)
+    else:
+        print(ele)
