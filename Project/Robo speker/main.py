2fb30fffbcddc902068218db89b464042fc4a4d4
myusername~ <kmohdhamza10@gmail.com>
2024-08-27 13:38:31 +0530

    Update a robo speaker game

--- Project/Robo speker/main.py
+++ Project/Robo speker/main.py
@@ -1,9 +1,13 @@
 import pyttsx3
+
 engine = pyttsx3.init()
 print("Welcome to robo speaker 1.1 Created by 'Hamza shaikh'")
+
 while True:
-    user_query=input("Enter what you want to speake?\n")
+    user_query = input("Enter what you want to speak?\n")
     engine.say(user_query)
-    if user_query=='q':
+
+    if user_query != 'q':
+        engine.runAndWait()  
+    else:
         break
-engine.runAndWait()
